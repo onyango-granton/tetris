37c95e68eb2e4342e9077b6342568b131e11d7b3
onyango-granton <grantononyango@gmail.com>
2024-07-16 16:02:54 +0300

    Feat: function to change . and # to 0 and 1

--- SortInputFile/main.go
+++ SortInputFile/main.go
@@ -7,3 +7,13 @@ import (
 	"strings"
 )
 
+func byteToInt(b byte) (int, error) {
+	if b == '.' {
+		return 0, nil
+	} else if b == '#' {
+		return 1, nil
+	} else {
+		return 0, errors.New("error in sample file")
+	}
+}
+
