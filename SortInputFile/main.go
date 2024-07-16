f255de1340355f2139c8cc41950e5136fa031c28
onyango-granton <grantononyango@gmail.com>
2024-07-16 16:05:17 +0300

    Feat: main function with some comments

--- SortInputFile/main.go
+++ SortInputFile/main.go
@@ -39,3 +39,79 @@ func stringToIntSlice(s string) ([]int,error){
 	}
 	return res, nil
 }
+
+func main() {
+	sampleFile, err := os.ReadFile("sample.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// var lines []string
+	var nums [][]int
+	for i, ch := range strings.Split(string(sampleFile), "\n") {
+		if ch == "" {
+			continue
+		}
+		chArr, err := stringToIntSlice(ch)
+		if err!= nil{
+			fmt.Println(err.Error(),"at line",i+1)
+			return
+		} else {
+			nums = append(nums, chArr)
+		}
+		// lines = append(lines, ch)
+	}
+	// fmt.Println(len(lines))
+
+	// alphabetMap := make(map[string][][]int)
+
+	// for i := 0; i < len(lines); {
+	// 	for j := i; j < i+4; j++ {
+	// 		fmt.Println(lines[j], i)
+	// 	}
+	// 	i += 4
+	// }
+
+	// for i:=0; i<len(nums);{
+
+	// 	for j:=i;j<i+4;j++{
+	// 		fmt.Println(lines[j],i)
+	// 	}
+	// 	i += 4
+	// }
+	
+	startAscii := 'A'
+	tetrominoes := make(map[rune][][]int)
+	// for _,ch := range nums{
+	// 	characterMino := [][]int{}
+	// 	for range 4{
+	// 		characterMino = append(characterMino, ch)
+	// 	}
+	// 	tetrominoes [startAscii] = characterMino
+	// 	startAscii++
+	// }
+
+
+	for i:=0; i<len(nums);{
+		characterMino := [][]int{}
+		for j:=i; j<i+4;j++{
+			characterMino = append(characterMino, nums[j])
+		}
+		tetrominoes[startAscii] = characterMino
+		startAscii ++
+		i += 4
+	}
+
+	for k,v := range tetrominoes{
+		fmt.Println(string(k),v)
+	}
+
+
+	// for i := 0; i < len(nums);{
+	// 	for j := i ; j < i+4;j++{
+	// 		fmt.Print(nums[j])
+	// 	}
+	// 	fmt.Println(" ",i)
+	// 	i += 4
+	// }
+}
